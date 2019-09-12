d007909fd480b227e6f553977f792584d6971f03
huynhlv <huynhlevan1997@gmail.com>
2019-09-12 11:16:27 +0700

    add cors method delete

--- config/application.rb
+++ config/application.rb
@@ -22,7 +22,7 @@ module SherpaApi
     config.middleware.insert_before 0, Rack::Cors do
       allow do
         origins '*'
-        resource '*', headers: :any, methods: [:get, :post, :options]
+        resource '*', headers: :any, methods: [:get, :post, :delete, :path, :put, :options]
       end
     end
     # Initialize configuration defaults for originally generated Rails version.
